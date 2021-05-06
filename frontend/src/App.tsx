c3a61ffa80c400bd181851f56c76c92a4631fce3
Rafael Borges <rafaelbertelli89@gmail.com>
2021-05-05 21:29:59 -0300

    create components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import Navbar from "components/Navbar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
