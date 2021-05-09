dd3bfdb164a5f72bdede6e7afbf02a1f98112194
Rafael Borges <rafaelbertelli89@gmail.com>
2021-05-09 10:08:15 -0300

    navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,18 +1,29 @@
+import { Link } from "react-router-dom";
+import Navbar from "components/Navbar";
+import Footer from "components/Footer";
+
 function Home() {
   return (
-    <div className="container">
-      <div className="jumbotron">
-        <h1 className="display-4">DSVendas</h1>
-        <p className="lead">
-          Analise o desempenho das suas vendas por diferentes perspectivas
-        </p>
-        <hr />
-        <p>
-          Esta aplicação consiste em exibir um dashboard a partir de dados
-          fornecidos por um back end construído com Spring Boot.
-        </p>
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link to="/dashboard" className="btn btn-primary btn-lg">
+            Acessar o Dashboard
+          </Link>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
